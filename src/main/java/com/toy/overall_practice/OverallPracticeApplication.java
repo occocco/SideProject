1054987b109c82887fd5283c9b65354fe6d9cf77
occocco <wcaqihgn@gmail.com>
2023-02-24 18:21:13 +0900

    first commit

---
+++ src/main/java/com/toy/overall_practice/OverallPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.toy.overall_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverallPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverallPracticeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/toy/overall_practice/config/SpringSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.toy.overall_practice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().disable()
+                .logout().disable();
+        return http.build();
+    }
+}
